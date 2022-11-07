f4b61d8eb1b5cc5b70140a0f2c4eebdf48cd849b
Lucie Wolf <lucie@LWMacbook.local>
2022-11-06 20:52:20 -0600

    Added database schema

---
+++ doc/database-schema.sql
@@ -0,0 +1,67 @@
+CREATE TABLE game (
+    id SERIAL,
+    title TEXT,
+    release_date DATE, 
+    english_support BOOLEAN,
+    windows_support BOOLEAN,
+    mac_support BOOLEAN,
+    linux_support BOOLEAN,
+    minimum_age INTEGER,
+    pos_ratings INTEGER,
+    neg_ratings INTEGER,
+    price FLOAT,
+    described TEXT,
+    website TEXT,
+)
+
+CREATE TABLE developer (
+    id SERIAL,
+    developer_name TEXT
+)
+
+CREATE TABLE publisher (
+    id SERIAL,
+    publisher_name TEXT
+)
+
+CREATE TABLE category (
+    id SERIAL,
+    category_name TEXT
+)
+
+CREATE TABLE genre (
+    id SERIAL,
+    genre_name TEXT
+)
+
+CREATE TABLE media (
+    id SERIAL,
+    media_link, TEXT
+)
+
+
+
+CREATE TABLE game_developer (
+    game_id, INTEGER,
+    developer_id INTEGER
+)
+
+CREATE TABLE game_publisher (
+    game_id, INTEGER,
+    publisher_id INTEGER
+)
+
+CREATE TABLE game_category (
+    game_id, INTEGER,
+    category_id INTEGER
+)
+
+CREATE TABLE game_genre (
+    game_id, INTEGER,
+    genre_id INTEGER
+)
+
+CREATE TABLE game_media (
+    game_id, INTEGER,
+    media_id INTEGER
+)
