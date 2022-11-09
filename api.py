4390e54d9cfb0e60cd311ec95e2a41426a52d063
Yuelin Kuang <kyl@Yuelins-MacBook-Pro.local>
2022-11-08 18:41:39 -0600

    Added template

---
+++ api.py
@@ -0,0 +1,65 @@
+'''
+    api.py
+    Lucie Wolf and Yuelin Kuang
+    8 Nov 2022
+
+    Web application project for CS257.
+'''
+
+import sys
+import flask
+import json
+import config
+import psycopg2
+
+api = flask.Blueprint('api', __name__)
+
+def get_connection():
+    ''' Returns a connection to the database described in the
+        config module. May raise an exception as described in the
+        documentation for psycopg2.connect. '''
+    return psycopg2.connect(database=config.database,
+                            user=config.user,
+                            password=config.password)
+
+@api.route('/authors/') 
+def get_authors():
+    ''' Returns a list of all the authors in our database. See
+        get_author_by_id below for description of the author
+        resource representation.
+
+        By default, the list is presented in alphabetical order
+        by surname, then given_name. You may, however, use
+        the GET parameter sort to request sorting by birth year.
+
+            http://.../authors/?sort=birth_year
+
+        Returns an empty list if there's any database failure.
+    '''
+    query = '''SELECT id, given_name, surname, birth_year, death_year
+               FROM authors ORDER BY '''
+
+    sort_argument = flask.request.args.get('sort')
+    if sort_argument == 'birth_year':
+        query += 'birth_year'
+    else:
+        query += 'surname, given_name'
+
+    author_list = []
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(query, tuple())
+        for row in cursor:
+            author = {'id':row[0],
+                      'given_name':row[1],
+                      'surname':row[2],
+                      'birth_year':row[3],
+                      'death_year':row[4]}
+            author_list.append(author)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+    return json.dumps(author_list)
---
+++ app.py
@@ -0,0 +1,7 @@
+'''
+    api.py
+    Lucie Wolf and Yuelin Kuang
+    8 Nov 2022
+
+    Web application project for CS257.
+'''
